27575d736de51f083502248a5d03bdc9e0e0699f
QJoly <quentinjoly@thoughtless.eu>
2023-04-02 15:32:09 +0200

    Création lanceur Dagger pour PR

--- .ci/run_dagger_pull_request.py
+++ .ci/run_dagger_pull_request.py
@@ -3,7 +3,6 @@ import sys
 
 import markdown_lint 
 import docusaurus_build 
-import multi_arch_build as docker_build
 
 multithreaded = False
 if __name__ == "__main__":
@@ -16,11 +15,9 @@ if __name__ == "__main__":
 
         markdown_lint_test = threading.Thread(target=markdown_lint.run_test, args=(event,), name="markdown_lint_test")
         docusaurus_build_test = threading.Thread(target=docusaurus_build.run_test, args=(event,), name="docusaurus_build_test")
-        docker_build_test = threading.Thread(target=docker_build.run_test, args=(event,), name="docker_build_test")
 
         markdown_lint_test.start()
         docusaurus_build_test.start()
-        docker_build_test.start()
 
         # Wait for all threads to finish
         for thread in threading.enumerate():
@@ -42,6 +39,4 @@ if __name__ == "__main__":
         print("Markdown lint finished")  
         anyio.run(docusaurus_build.docusaurus_build)
         print("Docusaurus build finished")
-        anyio.run(docker_build.docker_image_build)
-        print("Docker image build finished")
 
